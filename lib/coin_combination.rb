b3ab0d57bd2793e2121ee5f1f8e30d61ca2773c9
Rachael Berecka & Carlo De Gregorio <student@epicodus.com>
2015-08-06 14:49:46 -0700

    it returns all change! Wahoo!. 4 passing tests

--- lib/coin_combination.rb
+++ lib/coin_combination.rb
@@ -3,17 +3,19 @@ class Fixnum
     given_amount = self
     counter = 0
 
-    dimes = given_amount./(10)
-    after_dimes = given_amount%10
+    quarters = given_amount./(25)
+    after_quarters = given_amount % 25
+    dimes = after_quarters./(10)
+    after_dimes = after_quarters % 10
     nickles = after_dimes./(5)
-    after_nickles = after_dimes%5
+    after_nickles = after_dimes % 5
 
     until counter.== (after_nickles)
         counter = counter.+(1)
         pennies = counter
     end
 
-    answer = pennies.to_s + " pennies, " + nickles.to_s + " nickles, " + dimes.to_s + " dimes"
+    answer = pennies.to_s + " pennies, " + nickles.to_s + " nickles, " + dimes.to_s + " dimes, " + quarters.to_s + " quarters"
     answer
   end
 end
--- spec/coin_combination_spec.rb
+++ spec/coin_combination_spec.rb
@@ -3,15 +3,19 @@ require('coin_combination')
 
 describe('Fixnum#coin_combination') do
   it ('it makes change to pennies only') do
-    expect((4).coin_combination()).to(eq('4 pennies, 0 nickles, 0 dimes'))
+    expect((4).coin_combination()).to(eq('4 pennies, 0 nickles, 0 dimes, 0 quarters'))
   end
 
   it ('it makes change to nickles and pennies') do
-    expect((9).coin_combination()).to(eq("4 pennies, 1 nickles, 0 dimes"))
+    expect((9).coin_combination()).to(eq("4 pennies, 1 nickles, 0 dimes, 0 quarters"))
   end
 
   it ('it makes change to dimes, nickles, and pennies') do
-    expect((16).coin_combination()).to(eq("1 pennies, 1 nickles, 1 dimes"))
+    expect((16).coin_combination()).to(eq("1 pennies, 1 nickles, 1 dimes, 0 quarters"))
+  end
+
+  it ('it makes change to dimes, nickles, pennies and quarters') do
+    expect((41).coin_combination()).to(eq("1 pennies, 1 nickles, 1 dimes, 1 quarters"))
   end
 end
 
