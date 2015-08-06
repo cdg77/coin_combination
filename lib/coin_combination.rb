9812ab5bde278523597cb544eb82c58e92d3237e
Rachael Berecka & Carlo De Gregorio <student@epicodus.com>
2015-08-06 13:57:08 -0700

    now returns nickles and pennies, 2nd spec passed)

--- lib/coin_combination.rb
+++ lib/coin_combination.rb
@@ -1,11 +1,30 @@
 class Fixnum
   define_method(:coin_combination) do
     given_amount = self
-    counter =1
-    until counter.==(given_amount)
+    counter = 0
+
+    # until counter.== (4)
+    #     pennies = counter.+(1)
+    # end
+
+    # not_pennies = given_amount - pennies
+    #
+    # nickles = not_pennies./(5)
+    #
+
+    # until counter.==(given_amount % 5 == 0)
+    #   counter = counter.+(1)
+    #   nickles = counter
+    # end
+
+    nickles = given_amount./(5)
+    after_nickles = given_amount%5
+
+    until counter.==(after_nickles)
         counter = counter.+(1)
         pennies = counter
     end
-    pennies.to_s + " pennies"
+    answer = pennies.to_s + " pennies" + ", " + nickles.to_s + " nickles"
+    answer
   end
 end
--- spec/coin_combination_spec.rb
+++ spec/coin_combination_spec.rb
@@ -2,8 +2,11 @@ require('rspec')
 require('coin_combination')
 
 describe('Fixnum#coin_combination') do
-  # given 4 cents it will return 4 pennies
-  it ('it makes change to pennies only') do
-    expect((4).coin_combination()).to(eq('4 pennies'))
+  # it ('it makes change to pennies only') do
+  #   expect((4).coin_combination()).to(eq('4 pennies'))
+  # end
+
+  it ('it makes change to nickles and pennies') do
+    expect((9).coin_combination()).to(eq("4 pennies, 1 nickles"))
   end
 end
